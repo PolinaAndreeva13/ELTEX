55528de3ff547bf182eab7a66d028e93d8581602
AndreevaPolina13 <polya3192@yandex.ru>
2023-11-11 04:04:41 -0500

    Script_1 update

--- script1.sh
+++ script1.sh
@@ -4,15 +4,17 @@ counter=0
 
 function_sleep() {
 	echo "Running PID $!"
-	sleep 2
-	echo "Stop PID $!"
+	sleep 3
 }
 
-for i in {1..5}
+for i in {1..8}
 	do
 	function_sleep &
 	((counter++))
+	echo "Count of processes $counter" > res.txt
 	done
-wait
-echo "Count of processes $counter" > res.txt
-sleep 3600
+
+jobs
+sleep 15
+echo "Done!"
+jobs
