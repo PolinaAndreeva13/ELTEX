007e05e325b5c7371ec07d0226d08776d547ff7c
PolinaAndreeva13 <polya3192@yandex.ru>
2024-01-09 06:43:29 +0000

    Implementation of the cat command

---
+++ task7.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/syscall.h>
+#define PERMS 0777
+
+int main(int argc, char *argv[]){
+
+	if (argc != 3) {
+	
+		perror("Command operation: <command> <mode> <file_name>\nmode: 1 - read; 2 - write\n");
+		exit(1);
+
+	}
+
+	char buf[BUFSIZ];
+	size_t size;
+	
+	if (strcmp(argv[1], "1") == 0){
+
+		int fd = open(argv[2], O_RDONLY, PERMS);
+
+		if (fd == -1) {
+
+			perror("Error opened");
+			exit(1);
+
+		}
+	
+		while ((size = read(fd, buf, BUFSIZ)) > 0){
+	
+			if (write(1, &buf, size) != size){
+		
+				perror("FATAL");
+				exit(1);
+		
+			}	
+	
+		}
+
+		close(fd);
+
+	}
+
+	if (strcmp(argv[1], "2") == 0) {
+		
+		int fd = creat(argv[2], PERMS);
+
+		if (fd == -1) {
+		
+			perror("Error opened");
+			exit(1);
+
+		}
+
+		while((size = read(0, buf, BUFSIZ)) > 0) {
+	
+			write(fd, &buf, size);
+
+		}
+
+		close(fd);
+
+	}
+
+}
+
