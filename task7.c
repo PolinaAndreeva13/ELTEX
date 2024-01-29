aac9a5211333525dcce85101f69745e11759c017
PolinaAndreeva13 <polya3192@yandex.ru>
2024-01-29 09:14:31 +0000

    add functions

--- task7.c
+++ task7.c
@@ -7,45 +7,11 @@
 #include<sys/syscall.h>
 #define PERMS 0777
 
-int main(int argc, char *argv[]){
-
-	if (argc != 2) {
-	
-		perror("Command operation: <command> <file_name>\n");
-		exit(1);
-
-	}
+int print_cat(char *argv){
 
 	char buf[BUFSIZ];
-	size_t size;	
-
-	/*if (argc == 3){
-		
-		int fd = creat(argv[2], PERMS);
-
-		if (fd == -1) {
-		
-			perror("Error opened");
-			exit(1);
-
-		}
-
-		while((size = read(0, buf, BUFSIZ)) > 0) {
-	
-			if (write(fd, &buf, size) != size){
-			
-				perror("FATAL");
-				exit(1);
-
-			}
-
-		}
-
-		close(fd);
-
-	} else {*/
-	
-		int fd = open(argv[1], O_RDONLY, PERMS);
+	size_t size;
+	int fd = open(argv, O_RDONLY, PERMS);
 
 		if (fd == -1) {
 
@@ -66,7 +32,59 @@ int main(int argc, char *argv[]){
 		}
 
 		close(fd);
+
+		return 1;
+
+}
+
+int enter_cat(char *argv) {
 	
-//	} 
+	char buf[BUFSIZ];
+	size_t size;
+	int fd = creat(argv, PERMS);
+
+	if (fd == -1) {
+
+		perror("Error opened");
+		exit(1);
+
+		}
+
+	while((size = read(0, buf, BUFSIZ)) > 0) {
+
+		if (write(fd, &buf, size) != size){
+
+			perror("FATAL");
+			exit(1);
+
+		}
+
+	}
+		
+	close(fd);
+
+	return 1;
+
+}
+
+int main(int argc, char *argv[]){
+
+	if (argc != 2) {
+	
+		perror("Command operation: <command> <file_name>\n");
+		exit(1);
+
+	}	
+
+	/*if (argc == 3){
+	 
+	 	enter_cat(argv[2]);
+
+	} else {*/
+	
+		print_cat(argv[1]);	
+
+	
+	//} 
 	
 }
